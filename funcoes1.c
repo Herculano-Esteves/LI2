f41084b48c7ef8f9daacc560d0e07b7423d1e928
Herculano Esteves <pedroesteves005@gmail.com>
2024-03-13 11:03:48 +0000

    readme

---
+++ funcoes1.c
@@ -0,0 +1,5 @@
+#include <math.h>
+
+int soma(int x,int y){
+    return x + y;
+}
---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+#include "funcoes1.h"
+#include "funcoes1.c"
+
+
+int main() {
+    int x = soma(1,4);
+    printf("%d\n",x);
+    return 1;
+}
