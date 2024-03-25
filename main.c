efc8e33ee1cfe726aa03d7e8e55f50962259fc63
Herculano Esteves <pedroesteves005@gmail.com>
2024-03-25 11:30:14 +0000

    pequeno comeco

--- main.c
+++ main.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 #include <locale.h>
 #include <math.h>
+#include <wchar.h>
 #include "funcoes1.h"
 #include "funcoes1.c"
 
 
 int main() {
-    //aqui testem com scanf e printf como quiserem
+    setlocale(LC_CTYPE, "C.UTF-8");
+    wchar_t c;
+
+    scanf("%lc", &c);
+    
+    printf("Caractere lido: %lc\n", c);
     return 1;
 }
